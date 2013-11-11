befa13fe51564b028f05c0c89155f233e686b11b
Nikolay Kasyanov <corrmage@gmail.com>
2013-11-12 00:33:43 +0400

    Proper API endpoint (with https: scheme)

--- SoundCloudStream/constants.m
+++ SoundCloudStream/constants.m
@@ -11,7 +11,7 @@
 NSString *const CRTSoundcloudURLScheme = @"ru.corristo.soundcloudstream";
 
 NSString *const CRTSoundcloudConnectURLString = @"http://soundcloud.com/connect";
-NSString *const CRTSoundcloudEndpointURLString = @"http://api.soundcloud.com/";
+NSString *const CRTSoundcloudEndpointURLString = @"https://api.soundcloud.com/";
 
 NSString *const CRTSoundcloudClientID = @"d2ca18cb9684ed9303a2117d58c9c8cb";
 NSString *const CRTSoundcloudSecret = @"30234e3342c525804f93cc96ab0758a4";
